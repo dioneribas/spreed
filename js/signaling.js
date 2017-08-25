83f8bac8ade00af1e6e5eb9416008ddba3a124e8
Joachim Bauch <bauch@struktur.de>
2017-08-25 12:14:59 +0200

    Switch to new "resumeid" for resuming sessions.
    Signed-off-by: Joachim Bauch <bauch@struktur.de>

--- js/signaling.js
+++ js/signaling.js
@@ -499,13 +499,13 @@
 
 	StandaloneSignaling.prototype.sendHello = function() {
 		var msg;
-		if (this.sessionId) {
+		if (this.resumeId) {
 			console.log("Trying to resume session", this.sessionId);
 			msg = {
 				"type": "hello",
 				"hello": {
 					"version": "1.0",
-					"sessionid": this.sessionId
+					"resumeid": this.resumeId
 				}
 			};
 		} else {
@@ -531,9 +531,9 @@
 	StandaloneSignaling.prototype.helloResponseReceived = function(data) {
 		console.log("Hello response received", data);
 		if (data.type !== "hello") {
-			if (this.sessionId) {
+			if (this.resumeId) {
 				// Resuming the session failed, reconnect as new session.
-				this.sessionId = '';
+				this.resumeId = '';
 				this.sendHello();
 				return;
 			}
@@ -544,9 +544,10 @@
 			return;
 		}
 
-		var resumedSession = !!this.sessionId;
+		var resumedSession = !!this.resumeId;
 		this.connected = true;
 		this.sessionId = data.hello.sessionid;
+		this.resumeId = data.hello.resumeid;
 
 		var messages = this.pendingMessages;
 		this.pendingMessages = [];
